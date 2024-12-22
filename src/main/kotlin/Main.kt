fb8ecf0791c3c94ac4a2626e10c6622acead4aa3
Jorjai <jorge.are.fdz@gmail.com>
2024-12-22 14:51:02 +0100

    Reorganise and clean the code and imports

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,25 @@
 import java.awt.Point
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
-import javax.swing.*
+import javax.swing.JFrame
+import javax.swing.JPanel
+import javax.swing.WindowConstants
 
 enum class Side{TOP,BOTTOM,LEFT,RIGHT}
 
 fun main() {
     val panel = JPanel()
-    panel.addMouseListener(object : MouseAdapter() {
-        override fun mouseEntered(e: MouseEvent?) {
-            if (e != null) {
-                println(closestSide(e.point))
-            }
+    val frame = JFrame().apply {
+        setSize(600, 600)
+        setLocationRelativeTo(null)
+        defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+        isVisible = true
+        add(panel)
+    }
 
+    panel.addMouseListener(object : MouseAdapter() {
+        override fun mouseEntered(e: MouseEvent) {
+            println(closestSide(e.point))
         }
 
         /**
@@ -23,7 +30,6 @@ fun main() {
          *
          * @param p the point to evaluate.
          * @return the [Side] enum value representing the closest side.
-         * @throws RuntimeException if no side can be determined.
          */
         fun closestSide(p: Point): Side {
             val distances = mapOf(
@@ -33,17 +39,7 @@ fun main() {
                 Side.RIGHT to panel.width - p.x
             )
 
-            return distances.minByOrNull { it.value }?.key
-                ?: throw RuntimeException("Unexpected error: No side found") // This shouldn't happen
+            return distances.minByOrNull { it.value }!!.key
         }
     })
-
-    val frame = JFrame().apply {
-        setSize(600, 600)
-        setLocationRelativeTo(null)
-        defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-        add(panel)
-        isVisible = true
-    }
-
 }
