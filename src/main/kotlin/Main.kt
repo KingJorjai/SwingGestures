9f35d57ac5e3ef1383c63ebf9fad81c5d07ba18e
Jorjai <jorge.are.fdz@gmail.com>
2024-12-22 18:36:36 +0100

    Implement Component following the mouse inside JPanel
    A ChildFollowsMousePanel class can be created passing a Component as argument, which will be its child.
    When the mouse is inside this panel, the child Component will follow it.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,31 @@
+import java.awt.Color
+import java.awt.Component
+import java.awt.Dimension
 import java.awt.Point
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
 import javax.swing.JFrame
 import javax.swing.JPanel
+import javax.swing.SwingUtilities
 import javax.swing.WindowConstants
 
 enum class Side{TOP,BOTTOM,LEFT,RIGHT}
 
-fun main() {
-    val panel = JPanel()
-    val frame = JFrame().apply {
-        setSize(600, 600)
-        setLocationRelativeTo(null)
-        defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
-        isVisible = true
-        add(panel)
+class ChildFollowsMousePanel(private val child: Component): JPanel() {
+
+    inner class MouseMoveListener(): MouseAdapter() {
+        override fun mouseMoved(e: MouseEvent) {
+            val relativePoint = SwingUtilities.convertPoint(e.component, e.point, this@ChildFollowsMousePanel)
+
+            this@ChildFollowsMousePanel.
+            child.location = Point(relativePoint.x-child.size.width/2, relativePoint.y-child.height/2)
+        }
     }
 
-    panel.addMouseListener(object : MouseAdapter() {
+    inner class MouseEnterListener(): MouseAdapter() {
         override fun mouseEntered(e: MouseEvent) {
-            println(closestSide(e.point))
+            val relativePoint = SwingUtilities.convertPoint(e.component, e.point, this@ChildFollowsMousePanel)
+            println(closestSide(relativePoint))
         }
 
         /**
@@ -34,12 +40,36 @@ fun main() {
         fun closestSide(p: Point): Side {
             val distances = mapOf(
                 Side.TOP to p.y,
-                Side.BOTTOM to panel.height - p.y,
+                Side.BOTTOM to this@ChildFollowsMousePanel.height - p.y,
                 Side.LEFT to p.x,
-                Side.RIGHT to panel.width - p.x
+                Side.RIGHT to this@ChildFollowsMousePanel.width - p.x
             )
 
             return distances.minByOrNull { it.value }!!.key
         }
+    }
+
+    init {
+        addMouseMotionListener(MouseMoveListener())
+
+        child.addMouseMotionListener(MouseMoveListener())
+        child.addMouseListener(MouseEnterListener())
+
+        add(child)
+        layout = null
+    }
+}
+
+fun main() {
+    val panel = ChildFollowsMousePanel(JPanel().apply {
+        size = Dimension(150,50)
+        background = Color.black
     })
+    val frame = JFrame().apply {
+        add(panel)
+        setSize(600, 600)
+        setLocationRelativeTo(null)
+        defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
+        isVisible = true
+    }
 }
